74471aae5e5b6d1e5f41360b9e064467dbde1a08
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-05-02 16:56:11 +0100

    add relevant comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,9 @@
-const navToggle = document.querySelector('.nav__toggle');
-const list = document.querySelector('.nav__list')
+//simple script for toggling the nav menu
+const navToggle = document.querySelector(".nav__toggle")
+const list = document.querySelector(".nav__list")
 
-navToggle.addEventListener('click',toggleMenu);
+navToggle.addEventListener("click", toggleMenu)
 
-function toggleMenu(){
-    return list.classList.toggle('nav-open');
-}//this is a function declaration, therefore it is hoisted. That means I can call it before it is initialized.f
+function toggleMenu() {
+  return list.classList.toggle("nav-open")
+} //this is a function declaration, therefore it is hoisted. That means I can call it before it is initialized.f
